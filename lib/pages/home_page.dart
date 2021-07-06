a378937de0374b76c4b01cfa9e303d017cce2622
Preeti7766 <preeti7766@gmail.com>
2021-07-06 18:52:59 +0530

    day13 comeplete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,50 @@
+import 'package:first_app/models/catalog.dart';
 import 'package:first_app/widgets/drawer.dart';
+import 'dart:convert';
+import 'package:first_app/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Preeti";
+
+  @override
+  void initState() {
+    super.initState();
+    lodeData();
+  }
+
+  lodeData() async {
+    final catalogjson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogjson);
+    var productData = decodedData["products"];
+    print(productData);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catlog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("My First Android Application $days $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
